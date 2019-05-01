cd3f4d3a18255614ce8254745b08a4076c92b127
Mick Johnson <mick.johnson@homechef.com>
2019-04-30 22:57:31 -0500

    first pass at Mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,32 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// clear sum
+    @R2
+    M=0
+
+// initialize iterator
+    @i
+    M=0
+
+(LOOP)
+    @R0
+    D=M
+    @i
+    M=M+1 // i++
+    D=D-M
+    @END
+    D;JLT // exit loop if i-R0 < 0
+
+    // Meat of loop
+    @R1
+    D=M
+    @R2
+    M=D+M // sum = sum + adder
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
